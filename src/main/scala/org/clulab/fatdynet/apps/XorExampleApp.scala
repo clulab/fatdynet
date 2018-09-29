21b26b61950b966064e3ab7cfe741c53be57b94f
Keith Alcock <github@keithalcock.com>
2018-09-29 13:10:23 -0700

    Put all C++ code into resource

--- src/main/scala/org/clulab/fatdynet/apps/XorExampleApp.scala
+++ src/main/scala/org/clulab/fatdynet/apps/XorExampleApp.scala
@@ -1,13 +1,11 @@
 package org.clulab.fatdynet.apps
 
 import edu.cmu.dynet.examples.XorExample
-import org.clulab.fatdynet.utils.Utils
 
 object
  XorExampleApp {
 
   def main(args: Array[String]) {
-//    Utils.loadDynet()
     XorExample.main(args)
   }
 }
--- src/main/scala/org/clulab/fatdynet/apps/XorScalaApp.scala
+++ src/main/scala/org/clulab/fatdynet/apps/XorScalaApp.scala
@@ -1,12 +1,10 @@
 package org.clulab.fatdynet.apps
 
 import edu.cmu.dynet.examples.XorScala
-import org.clulab.fatdynet.utils.Utils
 
 object XorScalaApp {
 
   def main(args: Array[String]) {
-//    Utils.loadDynet()
     XorScala.main(args)
   }
 }
