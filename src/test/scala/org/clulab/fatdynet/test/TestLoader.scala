607c8c1fd99982abf328de26bea3c6034df4925d
Keith Alcock <github@keithalcock.com>
2018-12-18 15:03:33 -0700

    Standardize tests

--- src/test/scala/org/clulab/fatdynet/test/TestLoader.scala
+++ src/test/scala/org/clulab/fatdynet/test/TestLoader.scala
@@ -1,250 +1,162 @@
 package org.clulab.fatdynet.test
 
+import java.io.File
+
 import org.scalatest._
-
-import edu.cmu.dynet.{
-  Initialize,
-
-  Dim,
-  Expression,
-  IntVector,
-
-  ParameterCollection,
-
-  FastLstmBuilder,
-  LstmBuilder,
-    CompactVanillaLSTMBuilder,
-    CoupledLstmBuilder,
-    VanillaLstmBuilder,
-
-  // TreeLSTMBuilder, // abstract
-    UnidirectionalTreeLSTMBuilder,
-    BidirectionalTreeLSTMBuilder,
-
-  // RnnBuilder, // abstract
-    SimpleRnnBuilder,
-
-  GruBuilder
-}
-
+import edu.cmu.dynet._
 import org.clulab.fatdynet.utils.Closer.AutoCloser
 import org.clulab.fatdynet.utils.Loader
 import org.clulab.fatdynet.utils.Loader.ClosableModelSaver
 import org.clulab.fatdynet.utils.Transducer
 
 class TestLoader extends FlatSpec with Matchers {
-  Initialize.initialize(Map("random-seed" -> 2522620396l))
+
+  // TODO
+  // For LstmBuilder, CompactVanillaLSTMBuilder, CoupledLstmBuilder, VanillaLstmBuilder
+  //    newBuilder.setDropout(d = 0.12f, dR = 0.34f)
+  // should work, but might have to reset the RNG after the model is reloaded.
+  // Scala does not have access to this functionality.
+
+  // For SimpleRnnBuilder support
+  //     newBuilder.addAuxiliaryInput(x: Expression, aux: Expression): Expression
+  // rather than just the simple addInput.
+
+  abstract class LoaderTester(val layers: Int, val inputDim: Int, val hiddenDim: Int, val name: String) {
+    val testname: String = name + "_" +  layers + "_" + inputDim + "_" + hiddenDim
+    def makeBuilder(model: ParameterCollection): RnnBuilder
+    def loadBuilder: (Option[RnnBuilder], Option[ParameterCollection], Map[String, Expression])
+
+    val filename: String = "Test" + testname + ".txt"
+    val input: Expression = Expression.randomNormal(Dim(inputDim))
+    val inputs = Array(input)
+
+    def test: Unit = {
+      behavior of testname
+
+      it should "serialize the builder properly" in {
+        val oldModel = new ParameterCollection
+        val oldBuilder = makeBuilder(oldModel)
+        oldBuilder.newGraph()
+        oldBuilder.startNewSequence()
+        val oldTransduced = Transducer.transduce(oldBuilder, inputs)
+        val oldSum = Expression.sumElems(oldTransduced.get)
+
+        new ClosableModelSaver(filename).autoClose { saver =>
+          saver.addModel(oldModel)
+        }
+
+        val (optionBuilder, newOptionModel, _) = loadBuilder
+        val newBuilder = optionBuilder.get
+        val newModel = newOptionModel.get
+        newBuilder.newGraph()
+        newBuilder.startNewSequence()
+        val newTransduced = Transducer.transduce(newBuilder, inputs)
+        val newSum = Expression.sumElems(newTransduced.get)
+
+//        oldModel.parametersList.foreach { parameterStorage =>
+//          println(parameterStorage.dim)
+//        }
+//
+//        newModel.parametersList.foreach { parameterStorage =>
+//          println(parameterStorage.dim)
+//        }
+//
+//        println(oldSum.value.toFloat)
+//        println(newSum.value.toFloat)
+        oldSum.value.toFloat should be(newSum.value.toFloat)
+
+        new File(filename).delete
+      }
+    }
+  }
+
+  class FastLstmLoaderTester(layers: Int, inputDim: Int, hiddenDim: Int)
+      extends LoaderTester(layers, inputDim, hiddenDim, "FastLstmLoader") {
+
+    def makeBuilder(model: ParameterCollection): RnnBuilder = new FastLstmBuilder(layers, inputDim, hiddenDim, model)
+
+    def loadBuilder: (Option[RnnBuilder], Option[ParameterCollection], Map[String, Expression]) = Loader.loadFastLstm(filename)
+  }
+
+  class LstmLoaderTester(layers: Int, inputDim: Int, hiddenDim: Int, val lnLSTM: Boolean)
+      extends LoaderTester(layers, inputDim, hiddenDim, "LstmLoader" + "_" + lnLSTM) {
+
+    def makeBuilder(model: ParameterCollection): RnnBuilder = new LstmBuilder(layers, inputDim, hiddenDim, model, lnLSTM)
+
+    def loadBuilder: (Option[RnnBuilder], Option[ParameterCollection], Map[String, Expression]) = Loader.loadLstm(filename)
+  }
+
+  class CompactVanillaLstmLoaderTester(layers: Int, inputDim: Int, hiddenDim: Int)
+      extends LoaderTester(layers, inputDim, hiddenDim, "CompactVanillaLSTMLoader") {
+
+    def makeBuilder(model: ParameterCollection): RnnBuilder = new CompactVanillaLSTMBuilder(layers, inputDim, hiddenDim, model)
+
+    def loadBuilder: (Option[RnnBuilder], Option[ParameterCollection], Map[String, Expression]) = Loader.loadCompactVanillaLstm(filename)
+  }
+
+  class CoupledLstmLoaderTester(layers: Int, inputDim: Int, hiddenDim: Int)
+      extends LoaderTester(layers, inputDim, hiddenDim, "CoupledLstmLoader") {
+
+    def makeBuilder(model: ParameterCollection): RnnBuilder = new CoupledLstmBuilder(layers, inputDim, hiddenDim, model)
+
+    def loadBuilder: (Option[RnnBuilder], Option[ParameterCollection], Map[String, Expression]) = Loader.loadCoupledLstm(filename)
+  }
+
+  class VanillaLstmLoaderTester(layers: Int, inputDim: Int, hiddenDim: Int)
+      extends LoaderTester(layers, inputDim, hiddenDim, "VanillaLstmLoader") {
+
+    def makeBuilder(model: ParameterCollection): RnnBuilder = new VanillaLstmBuilder(layers, inputDim, hiddenDim, model)
+
+    def loadBuilder: (Option[RnnBuilder], Option[ParameterCollection], Map[String, Expression]) = Loader.loadVanillaLstm(filename)
+  }
+
+  class SimpleRnnLoaderTester(layers: Int, inputDim: Int, hiddenDim: Int, supportLags: Boolean)
+      extends LoaderTester(layers, inputDim, hiddenDim, "SimpleRnn" + "_" + supportLags) {
+
+    def makeBuilder(model: ParameterCollection): RnnBuilder = new SimpleRnnBuilder(layers, inputDim, hiddenDim, model, supportLags)
+
+    def loadBuilder: (Option[RnnBuilder], Option[ParameterCollection], Map[String, Expression]) = Loader.loadSimpleRnn(filename)
+  }
+
+  class GruLoaderTester(layers: Int, inputDim: Int, hiddenDim: Int)
+      extends LoaderTester(layers, inputDim, hiddenDim, "Gru") {
+
+    def makeBuilder(model: ParameterCollection): RnnBuilder = new GruBuilder(layers, inputDim, hiddenDim, model)
+
+    def loadBuilder: (Option[RnnBuilder], Option[ParameterCollection], Map[String, Expression]) = Loader.loadGru(filename)
+  }
+
+  Initialize.initialize(Map("random-seed" -> 2522620396L))
+
+  // Single tests of crashing combinations
+//  new FastLstmBuilderTester(3, 54, 22).test
+//  new LstmLoaderTester(4, 9, 16, lnLSTM = false).test
+//  new VanillaLstmLoaderTester(2, 9, 22).test
+
+  for (layers <- 1 to 4; inputDim <- 9 to 99 by 45; hiddenDim <- 10 to 22 by 6) {
+    new FastLstmLoaderTester(layers, inputDim, hiddenDim).test
+    new CompactVanillaLstmLoaderTester(layers, inputDim, hiddenDim).test
+    new CoupledLstmLoaderTester(layers, inputDim, hiddenDim).test
+    new VanillaLstmLoaderTester(layers, inputDim, hiddenDim).test
+    new GruLoaderTester(layers, inputDim, hiddenDim).test
+
+    new LstmLoaderTester(layers, inputDim, hiddenDim, lnLSTM = false).test
+    new LstmLoaderTester(layers, inputDim, hiddenDim, lnLSTM = true).test
+
+    new SimpleRnnLoaderTester(layers, inputDim, hiddenDim, supportLags = false).test
+    new SimpleRnnLoaderTester(layers, inputDim, hiddenDim, supportLags = true).test
+  }
+
 
   val layers = 1 // Loop over this for sure
   val inputDim = 99
   val hiddenDim = 22 // Must be even for bidirectional tree builder
-//  val input: Expression = Expression.parameter(new ParameterCollection().addParameters(Dim(inputDim)))
   val input: Expression = Expression.randomNormal(Dim(inputDim))
   val inputs = Array(input)
 
-  behavior of "FastLstmBuilder"
-
-  ignore should "load the builder with proper dimensions" in {
-    val filename = "TestFastLstmLoader.txt"
-    val oldModel = new ParameterCollection
-
-    // How to randomize these values?
-    val oldBuilder = new FastLstmBuilder(layers, inputDim, hiddenDim, oldModel)
-    oldBuilder.newGraph()
-    oldBuilder.startNewSequence()
-    val oldTransduced = Transducer.transduce(oldBuilder, inputs)
-    val oldSum = Expression.sumElems(oldTransduced.get)
-
-    new ClosableModelSaver(filename).autoClose { saver =>
-      saver.addModel(oldModel)
-    }
-
-    val (optionBuilder, newOptionModel, _) = Loader.loadFastLstm(filename)
-//    val newBuilder = optionBuilder.get
-    val newModel = newOptionModel.get
-//    newBuilder.newGraph()
-//    newBuilder.startNewSequence()
-//    val newTransduced = Transducer.transduce(newBuilder, inputs)
-//    val newSum = Expression.sumElems(newTransduced.get)
-
-    oldModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    newModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    println(oldSum.value.toFloat)
-//    println(newSum.value.toFloat)
-//    oldSum.value.toFloat should be (newSum.value.toFloat)
-  }
-
-  behavior of "LstmBuilder"
-
-  ignore should "load the builder with proper dimensions" in {
-    val filename = "TestLstmLoader.txt"
-    val oldModel = new ParameterCollection
-    val lnLSTM: Boolean = true // false is default, but also test true
-
-    val oldBuilder = new LstmBuilder(layers, inputDim, hiddenDim, oldModel, lnLSTM)
-    // TODO These don't work
-    //oldBuilder.setDropout(d = 0.12f, dR = 0.34f)
-    //oldBuilder.setDropoutMasks(batchSize = 5L)
-    oldBuilder.newGraph()
-    oldBuilder.startNewSequence()
-    val oldTransduced = Transducer.transduce(oldBuilder, inputs)
-    val oldSum = Expression.sumElems(oldTransduced.get)
-
-    new ClosableModelSaver(filename).autoClose { saver =>
-      saver.addModel(oldModel)
-    }
-
-    val (optionBuilder, newOptionModel, _) = Loader.loadLstm(filename)
-    val newBuilder = optionBuilder.get
-    // TODO These don't work
-    //newBuilder.setDropout(d = 0.12f, dR = 0.34f)
-    //newBuilder.setDropoutMasks(batchSize = 5L)
-    val newModel = newOptionModel.get
-    newBuilder.newGraph()
-    newBuilder.startNewSequence()
-    val newTransduced = Transducer.transduce(newBuilder, inputs)
-    val newSum = Expression.sumElems(newTransduced.get)
-
-    oldModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    newModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    println(oldSum.value.toFloat)
-    println(newSum.value.toFloat)
-    oldSum.value.toFloat should be (newSum.value.toFloat)
-  }
-
-  behavior of "CompactVanillaLSTMBuilder"
-
-  ignore should "load the builder with proper dimensions" in {
-    val filename = "TestCompactVanillaLSTMLoader.txt"
-    val oldModel = new ParameterCollection
-//    def setDropout(d: Float, dR: Float): Unit = builder.set_dropout(d, dR)
-//    def setDropoutMasks(batchSize:Long): Unit = builder.set_dropout_masks(batchSize)
-
-    val oldBuilder = new CompactVanillaLSTMBuilder(layers, inputDim, hiddenDim, oldModel)
-    oldBuilder.newGraph()
-    oldBuilder.startNewSequence()
-    val oldTransduced = Transducer.transduce(oldBuilder, inputs)
-    val oldSum = Expression.sumElems(oldTransduced.get)
-
-    new ClosableModelSaver(filename).autoClose { saver =>
-      saver.addModel(oldModel)
-    }
-
-    val (optionBuilder, newOptionModel, _) = Loader.loadCompactVanillaLstm(filename)
-    val newBuilder = optionBuilder.get
-    val newModel = newOptionModel.get
-    newBuilder.newGraph()
-    newBuilder.startNewSequence()
-    val newTransduced = Transducer.transduce(newBuilder, inputs)
-    val newSum = Expression.sumElems(newTransduced.get)
-
-    oldModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    newModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    println(oldSum.value.toFloat)
-    println(newSum.value.toFloat)
-    oldSum.value.toFloat should be (newSum.value.toFloat)
-  }
-
-  behavior of "CoupledLstmBuilder"
-
-  ignore should "load the builder with proper dimensions" in {
-    val filename = "TestCoupledLstmLoader.txt"
-    val oldModel = new ParameterCollection
-//    def setDropout(d: Float, dH: Float, dC: Float): Unit = builder.set_dropout(d, dH, dC)
-//    def setDropoutMasks(batchSize:Long): Unit = builder.set_dropout_masks(batchSize)
-
-    val oldBuilder = new CoupledLstmBuilder(layers, inputDim, hiddenDim, oldModel)
-    oldBuilder.newGraph()
-    oldBuilder.startNewSequence()
-    val oldTransduced = Transducer.transduce(oldBuilder, inputs)
-    val oldSum = Expression.sumElems(oldTransduced.get)
-
-    new ClosableModelSaver(filename).autoClose { saver =>
-      saver.addModel(oldModel)
-    }
-
-    val (optionBuilder, newOptionModel, _) = Loader.loadCoupledLstm(filename)
-    val newBuilder = optionBuilder.get
-    val newModel = newOptionModel.get
-    newBuilder.newGraph()
-    newBuilder.startNewSequence()
-    val newTransduced = Transducer.transduce(newBuilder, inputs)
-    val newSum = Expression.sumElems(newTransduced.get)
-
-    oldModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    newModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    println(oldSum.value.toFloat)
-    println(newSum.value.toFloat)
-    oldSum.value.toFloat should be (newSum.value.toFloat)
-  }
-
-  behavior of "VanillaLstmBuilder Loader"
-
-  ignore should "load the builder with proper dimensions" in {
-    val filename = "TestSimpleRnnLoader.txt"
-    val oldModel = new ParameterCollection
-    val lnLSTM: Boolean = true // false is default, but also test true
-//    def setDropout(d: Float, dR: Float): Unit = builder.set_dropout(d, dR)
-//    def setDropoutMasks(batchSize:Long): Unit = builder.set_dropout_masks(batchSize)
-
-    val oldBuilder = new VanillaLstmBuilder(layers, inputDim, hiddenDim, oldModel, lnLSTM)
-    oldBuilder.newGraph()
-    oldBuilder.startNewSequence()
-    val oldTransduced = Transducer.transduce(oldBuilder, inputs)
-    val oldSum = Expression.sumElems(oldTransduced.get)
-
-    new ClosableModelSaver(filename).autoClose { saver =>
-      saver.addModel(oldModel)
-    }
-
-    val (optionBuilder, newOptionModel, _) = Loader.loadVanillaLstm(filename)
-    val newBuilder = optionBuilder.get
-    val newModel = newOptionModel.get
-    newBuilder.newGraph()
-    newBuilder.startNewSequence()
-    val newTransduced = Transducer.transduce(newBuilder, inputs)
-    val newSum = Expression.sumElems(newTransduced.get)
-
-    oldModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    newModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    println(oldSum.value.toFloat)
-    println(newSum.value.toFloat)
-    oldSum.value.toFloat should be (newSum.value.toFloat)
-  }
-
   behavior of "UnidirectionalTreeLSTMBuilder"
 
-  it should "load the builder with proper dimensions" in {
+  ignore should "load the builder with proper dimensions" in {
     val filename = "TestUnidirectionalTreeLSTMLoader.txt"
     val oldModel = new ParameterCollection
 
@@ -319,85 +231,5 @@ class TestLoader extends FlatSpec with Matchers {
 //    println(oldSum.value.toFloat)
 //    println(newSum.value.toFloat)
 //    oldSum.value.toFloat should be (newSum.value.toFloat)
-  }
-
-  behavior of "SimpleRnnBuilder"
-
-  ignore should "load the builder with proper dimensions" in {
-    val filename = "TestSimpleRnnLoader.txt"
-    val oldModel = new ParameterCollection
-    val model = new ParameterCollection
-    val supportLags: Boolean = true
-//    def addAuxiliaryInput(x: Expression, aux: Expression): Expression = {
-
-    val oldBuilder = new SimpleRnnBuilder(layers, inputDim, hiddenDim, model, supportLags)
-    oldBuilder.newGraph()
-    oldBuilder.startNewSequence()
-    val oldTransduced = Transducer.transduce(oldBuilder, inputs)
-    val oldSum = Expression.sumElems(oldTransduced.get)
-
-    new ClosableModelSaver(filename).autoClose { saver =>
-      saver.addModel(model)
-    }
-
-    val (optionBuilder, newOptionModel, _) = Loader.loadSimpleRnn(filename)
-    val newBuilder = optionBuilder.get
-    newBuilder.newGraph()
-    val newModel = newOptionModel.get
-    newBuilder.newGraph()
-    newBuilder.startNewSequence()
-    val newTransduced = Transducer.transduce(newBuilder, inputs)
-    val newSum = Expression.sumElems(newTransduced.get)
-
-    oldModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    newModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    println(oldSum.value.toFloat)
-    println(newSum.value.toFloat)
-    oldSum.value.toFloat should be (newSum.value.toFloat)
-  }
-
-  behavior of "GruBuilder"
-
-  ignore should "load the builder with proper dimensions" in {
-    val filename = "TestGruLoader.txt"
-    val oldModel = new ParameterCollection
-    val model = new ParameterCollection
-
-    val oldBuilder = new GruBuilder(layers, inputDim, hiddenDim, model)
-    oldBuilder.newGraph()
-    oldBuilder.startNewSequence()
-    val oldTransduced = Transducer.transduce(oldBuilder, inputs)
-    val oldSum = Expression.sumElems(oldTransduced.get)
-
-    new ClosableModelSaver(filename).autoClose { saver =>
-      saver.addModel(model)
-    }
-
-    val (optionBuilder, newOptionModel, _) = Loader.loadGru(filename)
-    val newBuilder = optionBuilder.get
-    newBuilder.newGraph()
-    val newModel = newOptionModel.get
-    newBuilder.newGraph()
-    newBuilder.startNewSequence()
-    val newTransduced = Transducer.transduce(newBuilder, inputs)
-    val newSum = Expression.sumElems(newTransduced.get)
-
-    oldModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    newModel.parametersList.foreach { parameterStorage =>
-      println(parameterStorage.dim)
-    }
-
-    println(oldSum.value.toFloat)
-    println(newSum.value.toFloat)
-    oldSum.value.toFloat should be (newSum.value.toFloat)
   }
 }
