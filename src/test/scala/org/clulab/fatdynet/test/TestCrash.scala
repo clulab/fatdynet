811cf0c0404dd5c540696d54adbad1a91ffbcecd
Keith Alcock <github@keithalcock.com>
2019-05-13 12:05:43 -0700

    Streamline test more

--- src/test/scala/org/clulab/fatdynet/test/TestCrash.scala
+++ src/test/scala/org/clulab/fatdynet/test/TestCrash.scala
@@ -3,11 +3,12 @@ package org.clulab.fatdynet.test
 import java.io.File
 
 import edu.cmu.dynet._
-import org.scalatest._
+
+import org.scalatest.TestSuite
 
 import scala.io.Source
 
-class TestCrash extends FlatSpec with Matchers {
+class TestCrash extends TestSuite {
   Initialize.initialize(Map("random-seed" -> 2522620396L, "dynet-mem" -> "2048"))
 
   def asString(lookupParameter: LookupParameter, name: String): Unit = {
@@ -18,25 +19,26 @@ class TestCrash extends FlatSpec with Matchers {
     modelSaver.addLookupParameter(lookupParameter, name)
     modelSaver.done()
 
-    val string = Source.fromFile(filename).mkString // This seems critical
+    val string = Source.fromFile(filename).mkString
     tmpFile.delete()
   }
 
-  def testNamedParameter = {
+  def testParameter = {
     val oldParameterCollection = new ParameterCollection()
     val oldParameter = oldParameterCollection.addParameters(Dim(51))
     val oldParametersList = oldParameterCollection.parametersList
   }
 
-  def testNamedLookupParameter = {
+  def testLookupParameter = {
     val oldParameterCollection = new ParameterCollection()
     val oldLookupParameters = oldParameterCollection.addLookupParameters(1234, Dim(300))
+    // Must do this twice
     asString(oldLookupParameters, "/name")
     asString(oldLookupParameters, "/name")
   }
 
   // Must be in this order
-  testNamedParameter
+  testParameter
   // Only works if these are lookup parameters
-  testNamedLookupParameter
+  testLookupParameter
 }
