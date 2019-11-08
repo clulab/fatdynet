5e6b1de4815d857559b26d4480bfbd2bdcecf639
Keith Alcock <github@keithalcock.com>
2019-11-08 11:42:04 -0700

    Keep track of initialization arguments

--- src/main/scala/org/clulab/fatdynet/utils/Initializer.scala
+++ src/main/scala/org/clulab/fatdynet/utils/Initializer.scala
@@ -4,16 +4,21 @@ import edu.cmu.dynet._
 
 object Initializer {
   protected var initialized: Boolean = false
+  protected var mostRecentArgs: Option[Map[String, Any]] = None
 
   // Returns whether had already been initialized and doesn't do it again.
   def initialize(args: Map[String, Any] = Map.empty): Boolean = synchronized {
+    println(s"New arguments: $args")
     if (initialized) {
       println("Keith is ignoring duplicate initialization.")
+      if (mostRecentArgs.get != args)
+        println("New arguments do not match current arguments!")
       true
     }
     else {
       Initialize.initialize(args)
       initialized = true
+      mostRecentArgs = Some(args)
       false
     }
   }
