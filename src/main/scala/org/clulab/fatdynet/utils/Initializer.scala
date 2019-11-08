1b3045a36f15b259612bb02e39fc8c6bb8dd357f
Keith Alcock <github@keithalcock.com>
2019-11-08 12:26:57 -0700

    Cleanup better

--- src/main/scala/org/clulab/fatdynet/utils/Initializer.scala
+++ src/main/scala/org/clulab/fatdynet/utils/Initializer.scala
@@ -4,22 +4,19 @@ import edu.cmu.dynet._
 
 object Initializer {
   protected var initialized: Boolean = false
-  protected var mostRecentArgs: Option[Map[String, Any]] = None
 
-  // Returns whether had already been initialized and doesn't do it again.
+  protected def cleanup() {
+    internal.dynet_swig.cleanup()
+  }
+
+  // Returns whether had previously been initialized or not.
   def initialize(args: Map[String, Any] = Map.empty): Boolean = synchronized {
-    println(s"New arguments: $args")
-    if (initialized) {
-      println("Keith is ignoring duplicate initialization.")
-      if (mostRecentArgs.get != args)
-        println("New arguments do not match current arguments!")
-      true
-    }
-    else {
-      Initialize.initialize(args)
-      initialized = true
-      mostRecentArgs = Some(args)
-      false
-    }
+    val oldInitialized = initialized
+
+    if (initialized)
+      cleanup()
+    initialized = true
+    Initialize.initialize(args)
+    oldInitialized
   }
 }
