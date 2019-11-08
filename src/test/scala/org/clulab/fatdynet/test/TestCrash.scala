afb5eb8007c185f9e159c10ad6a62d24d93c26ed
Keith Alcock <github@keithalcock.com>
2019-11-08 08:23:56 -0700

    Rename non-test methods so they don't look like tests

--- src/test/scala/org/clulab/fatdynet/test/TestCrash.scala
+++ src/test/scala/org/clulab/fatdynet/test/TestCrash.scala
@@ -23,13 +23,13 @@ class TestCrash extends TestSuite {
     tmpFile.delete()
   }
 
-  def testParameter = {
+  def makeParameter = {
     val oldParameterCollection = new ParameterCollection()
     val oldParameter = oldParameterCollection.addParameters(Dim(51))
     val oldParametersList = oldParameterCollection.parametersList
   }
 
-  def testLookupParameter = {
+  def makeLookupParameter = {
     val oldParameterCollection = new ParameterCollection()
     val oldLookupParameters = oldParameterCollection.addLookupParameters(1234, Dim(300))
     // Must do this twice
@@ -38,7 +38,7 @@ class TestCrash extends TestSuite {
   }
 
   // Must be in this order
-  testParameter
+  makeParameter
   // Only works if these are lookup parameters
-  testLookupParameter
+  makeLookupParameter
 }
