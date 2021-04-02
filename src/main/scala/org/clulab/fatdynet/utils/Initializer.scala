e0f83bfbadca169484766515e702946259b14999
Keith Alcock <github@keithalcock.com>
2021-04-02 15:14:42 -0700

    Harden initializer

--- src/main/scala/org/clulab/fatdynet/utils/Initializer.scala
+++ src/main/scala/org/clulab/fatdynet/utils/Initializer.scala
@@ -3,6 +3,8 @@ package org.clulab.fatdynet.utils
 import edu.cmu.dynet._
 import edu.cmu.dynet.internal.dynet_swig.reset_rng
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 // According to devices.cc, "Devices cannot be deleted at the moment because
 // the destructor is protected."  Cleanup is therefore disallowed.
 // At the very least, it causes which crashes the test suite.
@@ -15,26 +17,30 @@ object Initializer {
   val AUTOBATCH = "autobatch"
   val PROFILING = "profiling"
 
-  protected var initialized: Boolean = false
+  protected val initialized: AtomicBoolean = new AtomicBoolean(false)
 
-  def cleanup(): Boolean = this.synchronized {
-    val oldInitialized = initialized
+  protected def rawCleanup(): Boolean = {
+    val oldInitialized = initialized.get
 
     if (oldInitialized) {
-//      internal.dynet_swig.cleanup()
-//      initialized = false
+      // internal.dynet_swig.cleanup()
+      // initialized = false
     }
     oldInitialized
   }
 
-  def isInitialized: Boolean = this.synchronized { initialized }
+  def cleanup(): Boolean = Synchronizer.withoutComputationGraph("Initializer.cleanup") {
+    rawCleanup()
+  }
+
+  def isInitialized: Boolean = initialized.get
 
   // Returns whether had previously been initialized or not.
-  def initialize(args: Map[String, Any] = Map.empty): Boolean = this.synchronized {
-    val oldInitialized = initialized
+  def initialize(args: Map[String, Any] = Map.empty): Boolean = Synchronizer.withoutComputationGraph("Initializer.initialize") {
+    val oldInitialized = rawCleanup()
 
-    cleanup()
     if (oldInitialized && args.contains(RANDOM_SEED)) {
+      println("It was already initialized")
       // The initialization would have been ignored,
       // so the random seed will be set explicitly.
       val seed = args(RANDOM_SEED).asInstanceOf[Long]
@@ -43,10 +49,9 @@ object Initializer {
       // Imitate normal initialization output.
       System.err.println(s"[dynet] random seed: $seed")
     }
-    Synchronizer.withoutComputationGraph("Initializer.initialize") {
-      Initialize.initialize(args)
-    }
-    initialized = true
+    // This is still required, even if it was previously initialized.  Why?
+    Initialize.initialize(args)
+    initialized.set(true)
     oldInitialized
   }
 }
