7fe903c4df137b3a0aae6e01ea2b77acadaef113
Keith Alcock <github@keithalcock.com>
2021-06-17 01:12:23 -0700

    Run all three default

--- src/test/scala/org/clulab/dynet/xor/TestXor.scala
+++ src/test/scala/org/clulab/dynet/xor/TestXor.scala
@@ -40,21 +40,23 @@ class TestXor extends Test {
   def test(name: String, f: Boolean => Float): Unit = {
     behavior of name
 
-//    it should "run" in {
-//      val loss = f(false)
-//
-//      loss should be(Xor.expectedLoss)
-//    }
+    it should "run" in {
+      val loss = f(false)
 
-//    it should "run serially" in {
-//      Range.inclusive(1, 8).foreach { _ =>
-//        val loss = f(false)
-//
-//        loss should be(Xor.expectedLoss)
-//      }
-//    }
+      loss should be(Xor.expectedLoss)
+    }
+
+    it should "run serially" in {
+      Range.inclusive(1, 8).foreach { _ =>
+        val loss = f(false)
+
+        loss should be(Xor.expectedLoss)
+      }
+    }
 
     it should "run in parallel" in {
+      // The other tests may have left a computation graph in the main thread.
+      ComputationGraph.reset()
       ThreadUtils.parallelize(Range.inclusive(1, 1000), 8).foreach { index =>
        val threadId = Thread.currentThread.getId
         println(s"Enter with index $index, threadId $threadId")
