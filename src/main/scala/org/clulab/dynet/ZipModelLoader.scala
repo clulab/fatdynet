85e37b177a869be00d08468454fcaf835c370f63
Keith Alcock <github@keithalcock.com>
2022-03-16 14:16:13 -0700

    Add Scala files from edu.cmu

---
+++ src/main/scala/org/clulab/dynet/ZipModelLoader.scala
@@ -0,0 +1,15 @@
+// Because this class must have access to model.model, which is marked
+// private[dynet], it be packaged here, even though it comes from clulab.
+package edu.cmu.dynet
+
+class ZipModelLoader private[dynet](loader: internal.ZipFileLoader) {
+  def this(filename: String, zipname: String) = { this(new internal.ZipFileLoader(filename, zipname))}
+
+  def close(): Unit = done()
+
+  def populateModel(model: ParameterCollection, key: String = ""): Unit = loader.populate(model.model, key)
+  def populateParameter(p: Parameter, key: String = ""): Unit = loader.populate(p.parameter, key)
+  def populateLookupParameter(p: LookupParameter, key: String = ""): Unit = loader.populate(p.lookupParameter, key)
+
+  def done(): Unit = loader.delete()
+}
